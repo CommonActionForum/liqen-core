e76e2b1ecc9cee3c13c05905c8781aced1fbfb1a
Carlos Saito <c.saito.style@gmail.com>
2017-03-14 12:27:00 +0100

    Add "target" property to Annotations

--- web/models/annotation.ex
+++ web/models/annotation.ex
@@ -2,6 +2,8 @@ defmodule Core.Annotation do
   use Core.Web, :model
 
   schema "annotations" do
+    field :target, Core.Target
+
     belongs_to :article, Core.Article
     belongs_to :user, Core.User, foreign_key: :author
 
@@ -13,8 +15,8 @@ defmodule Core.Annotation do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:article_id])
-    |> validate_required([:article_id])
+    |> cast(params, [:article_id, :target])
+    |> validate_required([:article_id, :target])
     |> foreign_key_constraint(:article_id)
   end
 end
