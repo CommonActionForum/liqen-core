5fd906ee134a7734496beec3a174223cfd3927cb
Carlos Saito <c.saito.style@gmail.com>
2017-03-14 11:29:50 +0100

    Fix Annotation View: render the object itself only

--- web/views/annotation_view.ex
+++ web/views/annotation_view.ex
@@ -2,11 +2,11 @@ defmodule Core.AnnotationView do
   use Core.Web, :view
 
   def render("index.json", %{annotations: annotations}) do
-    %{data: render_many(annotations, Core.AnnotationView, "annotation.json")}
+    render_many(annotations, Core.AnnotationView, "annotation.json")
   end
 
   def render("show.json", %{annotation: annotation}) do
-    %{data: render_one(annotation, Core.AnnotationView, "annotation.json")}
+    render_one(annotation, Core.AnnotationView, "annotation.json")
   end
 
   def render("annotation.json", %{annotation: annotation}) do
