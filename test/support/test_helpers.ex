85862d070081d9da23fc3bbb75504e9cacd5618f
Carlos Saito <c.saito.style@gmail.com>
2017-03-16 12:09:58 +0100

    Add test helpers

---
+++ test/support/test_helpers.ex
@@ -0,0 +1,12 @@
+defmodule Core.TestHelpers do
+  alias Core.Repo
+
+  def insert_users(attrs \\ %{}) do
+    changes = Map.merge(%{email: "john#{Base.encode16(:crypto.strong_rand_bytes(8))}@example.com",
+                          password: "secret"}, attrs)
+
+    %Core.User{}
+    |> Core.User.changeset(changes)
+    |> Repo.insert!()
+  end
+end
