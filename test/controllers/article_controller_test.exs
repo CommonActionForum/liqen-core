4e0708e4c5854a4180991f88499fd96ccf32aece
Carlos Saito <c.saito.style@gmail.com>
2017-03-16 12:11:26 +0100

    Add tests for Article: check authentication

--- test/controllers/article_controller_test.exs
+++ test/controllers/article_controller_test.exs
@@ -1,61 +1,24 @@
 defmodule Core.ArticleControllerTest do
   use Core.ConnCase
 
-  alias Core.Article
-  @valid_attrs %{body: "some content", title: "some content"}
-  @invalid_attrs %{}
-
-  setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  test "Requires user authentication on certain actions", %{conn: conn} do
+    Enum.each([
+      put(conn, article_path(conn, :update, "123", %{})),
+      post(conn, article_path(conn, :create, %{})),
+      delete(conn, article_path(conn, :delete, "123")),
+    ], fn conn ->
+      assert json_response(conn, 401)
+      assert conn.halted
+    end)
   end
 
-  test "lists all entries on index", %{conn: conn} do
-    conn = get conn, article_path(conn, :index)
-    assert json_response(conn, 200)["data"] == []
+  test "Do not require user authentication on certain actions", %{conn: conn} do
+    Enum.each([
+      get(conn, article_path(conn, :index)),
+      get(conn, article_path(conn, :show, "")),
+    ], fn conn ->
+      assert json_response(conn, 200)
+    end)
   end
 
-  test "shows chosen resource", %{conn: conn} do
-    article = Repo.insert! %Article{}
-    conn = get conn, article_path(conn, :show, article)
-    assert json_response(conn, 200)["data"] == %{"id" => article.id,
-      "title" => article.title,
-      "body" => article.body}
-  end
-
-  test "renders page not found when id is nonexistent", %{conn: conn} do
-    assert_error_sent 404, fn ->
-      get conn, article_path(conn, :show, -1)
-    end
-  end
-
-  test "creates and renders resource when data is valid", %{conn: conn} do
-    conn = post conn, article_path(conn, :create), article: @valid_attrs
-    assert json_response(conn, 201)["data"]["id"]
-    assert Repo.get_by(Article, @valid_attrs)
-  end
-
-  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, article_path(conn, :create), article: @invalid_attrs
-    assert json_response(conn, 422)["errors"] != %{}
-  end
-
-  test "updates and renders chosen resource when data is valid", %{conn: conn} do
-    article = Repo.insert! %Article{}
-    conn = put conn, article_path(conn, :update, article), article: @valid_attrs
-    assert json_response(conn, 200)["data"]["id"]
-    assert Repo.get_by(Article, @valid_attrs)
-  end
-
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    article = Repo.insert! %Article{}
-    conn = put conn, article_path(conn, :update, article), article: @invalid_attrs
-    assert json_response(conn, 422)["errors"] != %{}
-  end
-
-  test "deletes chosen resource", %{conn: conn} do
-    article = Repo.insert! %Article{}
-    conn = delete conn, article_path(conn, :delete, article)
-    assert response(conn, 204)
-    refute Repo.get(Article, article.id)
-  end
 end
