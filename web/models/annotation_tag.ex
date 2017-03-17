bcfcddedfc716cbcc8bf832bf5441ae629b0c387
Carlos Saito <c.saito.style@gmail.com>
2017-03-17 10:48:40 +0100

    Fix dependencies

--- web/models/annotation_tag.ex
+++ web/models/annotation_tag.ex
@@ -2,8 +2,8 @@ defmodule Core.AnnotationTag do
   use Core.Web, :model
 
   schema "annotations_tags" do
-    belongs_to :annotation, Annotation, primary_key: true
-    belongs_to :tag, Tag, primary_key: true
+    belongs_to :annotation, Core.Annotation, primary_key: true
+    belongs_to :tag, Core.Tag, primary_key: true
   end
 
   def changeset(struct, params \\ %{}) do
@@ -12,6 +12,5 @@ defmodule Core.AnnotationTag do
     |> validate_required([:annotation_id, :tag_id])
     |> foreign_key_constraint(:annotation_id)
     |> foreign_key_constraint(:tag_id)
-
   end
 end
