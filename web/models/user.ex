84013bd2a2cfed5c6b70944ba3109adf84ee51ef
Carlos Saito <c.saito.style@gmail.com>
2017-03-20 11:34:41 +0100

    Add permissions and roles in User model

--- web/models/user.ex
+++ web/models/user.ex
@@ -5,6 +5,8 @@ defmodule Core.User do
     field :email, :string
     field :crypted_password, :string
     field :password, :string, virtual: true
+    field :permissions, {:array, :string}
+    field :role, :string, virtual: true
     has_many :annotations, Core.Annotation, foreign_key: :author
 
     timestamps()
@@ -15,10 +17,12 @@ defmodule Core.User do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:email, :password])
-    |> validate_required([:email, :password])
+    |> cast(params, [:email, :password, :role])
+    |> validate_required([:email, :password, :role])
+    |> validate_inclusion(:role, ["beta_user"])
     |> unique_constraint(:email)
     |> put_pass_hash()
+    |> put_permissions()
   end
 
   defp put_pass_hash(changeset) do
@@ -29,5 +33,32 @@ defmodule Core.User do
       _ ->
         changeset
     end
+  end
+
+  defp put_permissions(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{role: role}} ->
+        permissions =
+          case role do
+            "root" -> ["super_root"]
+            "beta_user" -> [
+              "create_facts",
+              "read_authored_facts",
+              "update_authored_facts",
+              "delete_authored_facts",
+              "create_annotations",
+              "read_authored_annotations",
+              "update_authored_annotations",
+              "delete_authored_annotations",
+            ]
+          end
+        put_change(changeset, :permissions, permissions)
+      _ ->
+        changeset
+    end
+  end
+
+  def can?(user, permission) do
+    Enum.member?(user.permissions, permission)
   end
 end
