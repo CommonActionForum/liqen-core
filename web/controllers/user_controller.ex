a575a09cd6e06ea4e0d723ee73740815dccb49d6
Carlos Saito <c.saito.style@gmail.com>
2017-03-20 11:36:12 +0100

    Limit action: POST /users
    Allow that action only for users that have the "super_root" permission.

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,7 +1,9 @@
 defmodule Core.UserController do
   use Core.Web, :controller
-
   alias Core.User
+
+  plug Guardian.Plug.EnsureAuthenticated, %{handler: Core.Auth}
+  plug :authorize
 
   def create(conn, params) do
     changeset = User.changeset(%User{}, params)
@@ -16,6 +18,19 @@ defmodule Core.UserController do
         conn
         |> put_status(:unprocessable_entity)
         |> render(Core.ChangesetView, "error.json", changeset: changeset)
+    end
+  end
+
+  defp authorize(conn, _opts) do
+    user = Guardian.Plug.current_resource(conn)
+
+    if User.can?(user, "super_user") do
+      conn
+    else
+      conn
+      |> put_status(:forbidden)
+      |> render(Core.ErrorView, "403.json", %{})
+      |> halt()
     end
   end
 end
