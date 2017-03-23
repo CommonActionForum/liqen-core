7125eed43a6afadb7767537dc260bf0fa7669018
Carlos Saito <c.saito.style@gmail.com>
2017-03-23 18:42:53 +0100

    v0.3.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Core.Mixfile do
 
   def project do
     [app: :core,
-     version: "0.3.1",
+     version: "0.3.2",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
