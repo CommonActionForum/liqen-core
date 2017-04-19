c441d5328dfe9ccd51a893c1d07b30958b7f0853
Carlos Saito <c.saito.style@gmail.com>
2017-04-19 19:20:20 +0200

    Include "required" field in Question-Tag relationship

--- web/models/question_tag.ex
+++ web/models/question_tag.ex
@@ -4,12 +4,14 @@ defmodule Core.QuestionTag do
   schema "questions_tags" do
     belongs_to :question, Core.Question, primary_key: true
     belongs_to :tag, Core.Tag, primary_key: true
+
+    field :required, :boolean
   end
 
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:question_id, :tag_id])
-    |> validate_required([:question_id, :tag_id])
+    |> cast(params, [:question_id, :tag_id, :required])
+    |> validate_required([:question_id, :tag_id, :required])
     |> foreign_key_constraint(:question_id)
     |> foreign_key_constraint(:tag_id)
   end
