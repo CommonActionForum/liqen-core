64423d999f1ab00f5e254a5d8b3cb49f182ad6b0
Carlos Saito <c.saito.style@gmail.com>
2017-04-24 10:26:48 +0200

    Update model Question (param "tags" is now "answer")

--- web/models/question.ex
+++ web/models/question.ex
@@ -3,9 +3,9 @@ defmodule Core.Question do
 
   schema "questions" do
     field :title, :string
-    field :tags, {:array, :map}, virtual: true
-
     many_to_many :question_tags, Core.Tag, join_through: Core.QuestionTag
+
+    field :answer, {:array, :map}, virtual: true
 
     timestamps()
   end
@@ -15,7 +15,19 @@ defmodule Core.Question do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:title, :tags])
-    |> validate_required([:title, :tags])
+    |> cast(params, [:title, :answer])
+    |> validate_required([:title, :answer])
+    |> validate_change(:answer, fn(:answer, tags) ->
+      tags
+      |> Enum.map(&validate_tag/1)
+      |> Enum.filter(fn tag -> tag != :ok end)
+    end)
+  end
+
+  defp validate_tag(%{tag: tag, required: required}) do
+    :ok
+  end
+  defp validate_tag(_) do
+    {:tags, {"Missing fields: tag and required, %{index}", [index: 1]}}
   end
 end
