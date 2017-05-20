ab1e8a156ce83c7c2ea8ab00418076bed8f94e55
Carlos Saito <c.saito.style@gmail.com>
2017-05-20 19:49:19 +0200

    Update manifest location

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :core, Core.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: System.get_env("APP_URL_HOST"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure Database
