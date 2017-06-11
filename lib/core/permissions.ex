45eb1dfc322eb9afa54f30025efad186528f7f4a
Carlos Saito <c.saito.style@gmail.com>
2017-06-11 15:40:11 +0200

    Migrate User model

---
+++ lib/core/permissions.ex
@@ -0,0 +1,68 @@
+defmodule Core.Permissions do
+  @moduledoc """
+  Operations with users permissions
+  """
+  use Core.Web, :model
+
+  alias Core.Accounts.User
+
+  @doc """
+  Put permissions to a changeset
+  """
+  def put_permissions(changeset = %Ecto.Changeset{valid?: true, changes: %{role: role}}) do
+    permissions =
+      case role do
+        "root" -> ["super_user"]
+        "beta_user" -> [
+          "show_users",
+          "create_facts",
+          "read_facts",
+          "update_facts",
+          "delete_facts",
+          "create_annotations",
+          "read_annotations",
+          "update_annotations",
+          "delete_annotations",
+        ]
+        _ -> []
+      end
+
+    put_change(changeset, :permissions, permissions)
+  end
+  def put_permissions(changeset), do: changeset
+
+  @doc """
+  Check if a `user` has a `permission`
+
+  Generally instead of using this, use `can?/3` and `can?/4`
+  """
+  def can?(user = %User{}, permission) do
+    Enum.member?(user.permissions, "super_user") or
+    Enum.member?(user.permissions, permission)
+  end
+
+  @doc """
+  Checks if a `user` has permissions to do an `action` to a resource of a `type`
+  """
+  def can?(user, "create", type) do
+    can?(user, "create_#{type}")
+  end
+  def can?(user, action, type) do
+    can?(user, "#{action}_all_#{type}")
+  end
+
+  @doc """
+  Checks if a `user` has permissions to do an `action` to a `resource` of a
+  `type`
+  """
+  def can?(user, action, "users", object) do
+    can?(user, action, "users") or (
+      can?(user, "#{action}_users") and object.id === user.id
+    )
+  end
+  def can?(user, action, type, object) do
+    can?(user, action, type) or (
+      can?(user, "#{action}_#{type}") and object.author === user.id
+    )
+  end
+end
--- lib/core/web/controllers/session_controller.ex
+++ lib/core/web/controllers/session_controller.ex
@@ -1,24 +1,16 @@
 defmodule Core.Web.SessionController do
   use Core.Web, :controller
 
-  alias Core.Repo
-  alias Core.Auth
+  alias Core.Registration
 
   def create(conn, %{"email" => email,
                      "password" => password}) do
 
-    case Auth.login(conn, email, password, repo: Repo) do
-      {:ok, conn} ->
-        jwt = conn.assigns.current_session.jwt
-        expires = conn.assigns.current_session.exp
+    case Registration.login_user(email, password) do
+      {:ok, user} ->
+        set_session(conn, user)
 
-        conn
-        |> put_resp_header("authorization", "Bearer #{jwt}")
-        |> put_resp_header("x-expires", to_string(expires))
-        |> put_status(:ok)
-        |> render("ok.json", session: conn.assigns.current_session)
-
-      {:error, _reason, _conn} ->
+      :error ->
         conn
         |> put_status(:unauthorized)
         |> render("unauthorized.json", %{})
@@ -26,5 +18,26 @@ defmodule Core.Web.SessionController do
   end
 
   def delete(_conn, _params) do
+  end
+
+  defp set_session(conn, user) do
+    new_conn = Guardian.Plug.api_sign_in(conn, user)
+    jwt = Guardian.Plug.current_token(new_conn)
+
+    case Guardian.Plug.claims(new_conn) do
+      {:ok, claims} ->
+        session = %{user: user, jwt: jwt, exp: Map.get(claims, "exp")}
+
+        conn
+        |> put_resp_header("authorization", "Bearer #{jwt}")
+        |> put_resp_header("x-expires", to_string(session.exp))
+        |> put_status(:ok)
+        |> render("ok.json", session: session)
+
+      {:error, _} ->
+        conn
+        |> put_status(:internal_server_error)
+        |> render("500.json", %{})
+    end
   end
 end
