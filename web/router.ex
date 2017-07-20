901c013defc08d3fe49832698aa0149294798ebd
Carlos Saito <c.saito.style@gmail.com>
2017-07-20 10:03:34 +0200

    Add /v3/... routes

--- web/router.ex
+++ web/router.ex
@@ -26,6 +26,17 @@ defmodule Core.Router do
     resources "/questions", QuestionController, except: [:new, :edit]
     resources "/liqens", FactController, except: [:new, :edit]
 
+    scope "/v3", V3, as: :v3 do
+      resources "/annotations", TagController, except: [:new, :edit]
+      resources "/articles", TagController, except: [:new, :edit]
+      resources "/liqens", TagController, except: [:new, :edit]
+      resources "/questions", TagController, except: [:new, :edit]
+      resources "/tags", TagController, except: [:new, :edit]
+      resources "/users", UserController, only: [:create, :show]
+
+      resources "/sessions", SessionController, only: [:create, :delete]
+    end
+
     get "/private", PrivateController, :example
   end
 
