1507f7203d2cbc86847a26a185088131cf721b5f
Carlos Saito <c.saito.style@gmail.com>
2017-07-20 12:55:14 +0200

    Drop UserController.find/2 plug

--- lib/core/web/controllers/v3/user_controller.ex
+++ lib/core/web/controllers/v3/user_controller.ex
@@ -28,18 +28,4 @@ defmodule Core.Web.V3.UserController do
       |> render("show.json", user: user)
     end
   end
-
-  defp find(conn = %Plug.Conn{params: %{"id" => id}}, _opts) do
-    case Core.Accounts.get_user(id) do
-      {:ok, user} ->
-        conn
-        |> assign(:user, user)
-
-      {:error, :not_found} ->
-        conn
-        |> put_status(:not_found)
-        |> render(Core.Web.ErrorView, "404.json", %{})
-        |> halt()
-    end
-  end
 end
