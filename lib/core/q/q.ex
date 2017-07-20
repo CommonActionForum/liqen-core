41f75df4b24b8481040a290a398fd9c569aec888
Carlos Saito <c.saito.style@gmail.com>
2017-07-20 17:58:10 +0200

    Define the Q interface

---
+++ lib/core/q/q.ex
@@ -0,0 +1,98 @@
+defmodule Core.Q do
+  @moduledoc """
+  Provides functions to get one, get all, create, update and delete:
+
+  - Questions
+  - Annotations
+  - Liqens
+  - Tags
+
+  All similar functions has the same pattern (where XXX is question, annotation
+  or liqen)
+
+  - get_XXX(id)
+
+    Gets an object. Returns {:ok, object} or {:error, :not_found}
+
+  - get_all_XXX()
+
+    Gets all objects. Returns a list of objects
+
+  - create_XXX(author, params)
+
+    Creates an object. Returns {:ok, object}, {:error, changeset} or
+    {:error, :forbidden}
+
+  - update_XXX(author, id, params)
+
+    Updates an object. Returns {:ok, object}, {:error, :not_found},
+    {:error, changeset} or {:error, :forbidden}
+
+  - delete_XXX(author, id, params)
+
+    Deletes an object. Returns {:ok, object}, {:error, :not_found} or
+    {:error, :forbidden}
+  """
+
+  alias Core.Repo
+
+  def get_question(id) do
+  end
+
+  def get_annotation(id) do
+  end
+
+  def get_liqen(id) do
+  end
+
+  def get_tag(id) do
+  end
+
+  def get_all_questions() do
+  end
+
+  def get_all_annotations() do
+  end
+
+  def get_all_liqens() do
+  end
+
+  def get_all_tags() do
+  end
+
+  def create_question(author, params) do
+  end
+
+  def create_annotation(author, params) do
+  end
+
+  def create_liqen(author, params) do
+  end
+
+  def create_tag(author, params) do
+  end
+
+  def update_question(author, id, params) do
+  end
+
+  def update_annotation(author, id, params) do
+  end
+
+  def update_liqen(author, id, params) do
+  end
+
+  def update_tag(author, id, params) do
+  end
+
+  def delete_question(author, id) do
+  end
+
+  def delete_annotation(author, id) do
+  end
+
+  def delete_liqen(author, id) do
+  end
+
+  def delete_tag(author, id) do
+  end
+end
