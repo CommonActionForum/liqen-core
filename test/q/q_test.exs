1e4a065e5f42a1738e53aaf8fd0f19f459d41c54
Carlos Saito <c.saito.style@gmail.com>
2017-07-21 18:37:47 +0200

    Change spec when deleting tags must go wrong
    When deleting a tag (giving its ID) and that tag is used in a question:
    
    - Before, Q.delete_tag returned a {:error, changeset}
    - Now, Q.delete_tag returns a {:error, :bad_request, string}

--- test/q/q_test.exs
+++ test/q/q_test.exs
@@ -138,7 +138,7 @@ defmodule Core.QTest do
     assert {:error, :not_found} = Q.delete_tag(root, 0)
   end
 
-  test "Delete tag breaking ref. integrity", %{root: root, tags: [_, t2, _, _, _]} do
-    assert {:error, changeset} = Q.delete_tag(root, t2.id)
+  test "Delete tag included in a question", %{root: root, tags: [_, t2, _, _, _]} do
+    assert {:error, :bad_request, _} = Q.delete_tag(root, t2.id)
   end
 end
